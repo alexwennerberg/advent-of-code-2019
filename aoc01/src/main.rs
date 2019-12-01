80711c2e57cb857078148a255d81565d038b3cfa
alex wennerberg <awennerberg@cloudbakers.com>
2019-12-01 00:27:47 -0600

    Playing around with performance improvements

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,28 +1,32 @@
 /// Solution to Day 1: https://adventofcode.com/2019/day/1
 
-use std::io::{self, Read};
+use std::io::{self};
+use std::io::prelude::*;
 
 fn main() {
     // Determine the total fuel requirements for a list of modules based on their mass
     // Given the "base" (Part 1) fuel calculation and "sophisticated" (Part 2) calculation
     // Input is a list of module masses (positive integers) separated by line breaks
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let fuel_functions: Vec<fn(i32) -> i32> = vec![base_calculate_fuel, sophisticated_calculate_fuel];
-    for (i, fuel_function) in fuel_functions.iter().enumerate() {
-        let result: i32 = input.lines().map(|line| fuel_function(line.parse::<i32>().unwrap())).sum();
-        println!("part {}: {}", i+1, result);
+    // Overflows if total mass >=  2^32
+    let mut partone: u32 = 0;
+    let mut parttwo: u32 = 0;
+    for line in io::stdin().lock().lines() {
+        let parsed = line.unwrap().parse::<u32>().unwrap();
+        partone += base_calculate_fuel(parsed);
+        parttwo += sophisticated_calculate_fuel(parsed);
     }
+    println!("part 1: {}", partone);
+    println!("part 2: {}", parttwo);
 }
 
-fn base_calculate_fuel(mass: i32) -> i32 {
+fn base_calculate_fuel(mass: u32) -> u32 {
     // (Part 1)
     // Fuel required to launch a given module is based on its mass. Specifically, to find the fuel
     // required for a module, take its mass, divide by three, round down, and subtract 2.
     return (mass / 3) - 2
 }
 
-fn sophisticated_calculate_fuel(mass: i32) -> i32 {
+fn sophisticated_calculate_fuel(mass: u32) -> u32 {
     // (Part 2)
     // Fuel itself requires fuel just like a module - take its mass, divide by three, round down,
     // and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so
