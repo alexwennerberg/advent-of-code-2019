4c8081a2eb1ef280518866ca1bf4fbc516e184f6
alex wennerberg <awennerberg@cloudbakers.com>
2019-11-30 23:48:01 -0600

    Refactor in functional style

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -3,39 +3,23 @@ use std::io::{self, Read};
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    partone(&input);
-    parttwo(&input);
-
+    let fuel_functions: Vec<fn(i32) -> i32> = vec![base_calculate_fuel, sophisticated_calculate_fuel];
+    for (i, fuel_function) in fuel_functions.iter().enumerate() {
+        let result: i32 = input.lines().map(|line| fuel_function(line.parse::<i32>().unwrap())).sum();
+        println!("part {}: {}", i+1, result);
+    }
 }
 
-fn partone_calculate_fuel(mass: i32) -> i32 {
+fn base_calculate_fuel(mass: i32) -> i32 {
     return (mass / 3) - 2
 }
 
-fn parttwo_calculate_fuel(mass: i32) -> i32 {
+fn sophisticated_calculate_fuel(mass: i32) -> i32 {
     let mut total = 0;
-    let mut current = mass / 3 - 2;
+    let mut current = base_calculate_fuel(mass);
     while current > 0 {
         total += current;
-        current = current / 3 -2;
+        current = base_calculate_fuel(current);
     }
     return total
-}
-
-fn partone(input: &String) {
-    let mut total_fuel = 0;
-    for line in input.lines() {
-        let mass: i32 = line.parse().unwrap();
-        total_fuel += partone_calculate_fuel(mass);
-    }
-    println!("part 1: {}", total_fuel);
-}
-
-fn parttwo(input: &String) {
-    let mut total_fuel = 0;
-    for line in input.lines() {
-        let mass: i32 = line.parse().unwrap();
-        total_fuel += parttwo_calculate_fuel(mass);
-    }
-    println!("part 2: {}", total_fuel);
 }
