571270da8539428019c6514d9e218f87fd9c402f
alex wennerberg <awennerberg@cloudbakers.com>
2019-12-01 17:06:10 -0600

    Remove comments

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -20,19 +20,10 @@ fn main() {
 }
 
 fn base_calculate_fuel(mass: u32) -> u32 {
-    // (Part 1)
-    // Fuel required to launch a given module is based on its mass. Specifically, to find the fuel
-    // required for a module, take its mass, divide by three, round down, and subtract 2.
     return (mass / 3) - 2
 }
 
 fn sophisticated_calculate_fuel(mass: u32) -> u32 {
-    // (Part 2)
-    // Fuel itself requires fuel just like a module - take its mass, divide by three, round down,
-    // and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so
-    // on. Any mass that would require negative fuel should instead be treated as if it requires
-    // zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has
-    // no mass and is outside the scope of this calculation.
     let mut total = 0;
     let mut current = base_calculate_fuel(mass);
     while current > 0 {
