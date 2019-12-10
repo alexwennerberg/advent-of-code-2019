5961ae60b4f4977e83383df3d9f0ae7846ce47ad
alex wennerberg <awennerberg@cloudbakers.com>
2019-12-10 12:54:22 -0600

    Day 10 part 1

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -4,50 +4,67 @@ use std::collections::HashSet;
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let asteroids: HashSet<(usize,usize)> = input
+    let asteroids: HashSet<(i32,i32)> = input
         .lines()
         .enumerate()
         .map(|(i, line)| line
              .chars()
              .enumerate()
              .filter(|(_, space)| *space == '#')
-             .map(move |(j, _)| (j,i)))
+             .map(move |(j, _)| (j as i32 ,i as i32 )))
         .flatten()
         .collect();
 
     let best = asteroids
         .iter()
-        .max_by_key(|a1| {
+        .map(|a1| {
             let count = asteroids.iter().filter(
                 |a2| {
-                    **a2 != **a1 && asteroids.iter().filter(|a3|{
-                        check_if_between(**a1,**a2,**a3)
-                           }).count() > 1 // includes self
+                    *a2 != a1 && asteroids.iter().filter(|a3|{
+                        let result = check_if_between(*a1,**a2,**a3);
+                        result
+                           }).count() == 0 // includes self
                 }
                 ).count();
-            println!("Count for {:?}: {}", a1, count);
             count
-        });
+        }).max().unwrap();
     println!("{:?}", best);
 }
 
-fn check_if_between(a: (usize, usize), b: (usize, usize), c: (usize, usize)) -> bool {
-    // checks if c is between a and b
-    let xdiffab = b.0 as i32 - a.0 as i32 ;
-    let ydiffab = b.1 as i32 - a.1 as i32;
-    let xdiffac = c.0 as i32 - a.0 as i32;
-    let ydiffac = c.1  as i32- a.1 as i32;
-    // flat special case
-    if xdiffab == 0 || xdiffac == 0 {
-        return xdiffab == xdiffac
+fn gcd(mut m: i32, mut n: i32) -> i32 {
+    // modified for 0
+    if n == 0 {
+        return m.abs()
     }
-    if ydiffab == 0 || ydiffac == 0 {
-        return xdiffab == xdiffac
+    else if m == 0 {
+        return n.abs()
     }
-    let slopeac = (ydiffac as f32/xdiffac as f32);
-    let slopeab = (ydiffab as f32/xdiffab as f32);
-    return slopeac == slopeab
+    while m != 0 {
+        let old_m = m;
+        m = n % m;
+        n = old_m;
+        }
+    n.abs()
+    }
 
+fn check_if_between(a: (i32, i32), b: (i32, i32), c: (i32, i32)) -> bool {
+    if c == a || c == b {
+        return false
+    }
+    let badiff = (b.0  -a.0  , b.1 -a.1 );
+    let gcd = gcd(badiff.0, badiff.1);
+    let step = (badiff.0 / gcd, badiff.1 / gcd);
+    let mut tracer = (a.0 , a.1 );
+    for _ in 0..=gcd {
+        tracer = (tracer.0  + step.0, tracer.1  + step.1);
+        if tracer == (c.0 , c.1 ) {
+            return true
+        }
+        else if tracer == (b.0 , b.1 ) {
+            return false;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -81,5 +98,41 @@ mod tests {
     let b=(0,2);
     let c=(0,1);
     assert!(check_if_between(a, b, c))
+    }
+    #[test]
+    fn test_0_slope() {
+    let a=(0,0);
+    let b=(2,0);
+    let c=(1,0);
+    assert!(check_if_between(a, b, c))
+    }
+    #[test]
+    fn test_b_is_c() {
+    let a=(0,0);
+    let b=(0,2);
+    let c=(0,2);
+    assert!(!check_if_between(a, b, c));
+    }
+
+    #[test]
+    fn test_a_is_c() {
+    let a=(0,0);
+    let b=(0,2);
+    let c=(0,0);
+    assert!(!check_if_between(a, b, c))
+    }
+    #[test]
+    fn test_broken_case() {
+    let a=(3,2);
+    let b=(1,2);
+    let c=(4,2);
+    assert!(!check_if_between(a, b, c))
+    }
+    #[test]
+    fn test_broken_case2() {
+    let a=(1,2);
+    let b=(4,2);
+    let c=(0,2);
+    assert!(!check_if_between(a, b, c))
     }
 }
