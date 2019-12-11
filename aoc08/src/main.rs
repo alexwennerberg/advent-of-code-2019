f6bf4af072ab78522420a23f8d63050f6d1802cb
alex wennerberg <awennerberg@cloudbakers.com>
2019-12-10 23:02:37 -0600

    day 8 part 2

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -3,7 +3,9 @@ use std::io::{self, Read};
 // Refactored based on https://github.com/sethetter/aoc-2019/blob/master/day-08/src/main.rs after
 // completing
 fn main() {
-    let layer_size = 25 * 6;
+    let height = 6;
+    let width = 25;
+    let layer_size = height * width;
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     input.pop();
@@ -19,7 +21,50 @@ fn main() {
         .min_by(|a, b|  count_digit(a, 0).cmp(&count_digit(b, 0)))
         .unwrap();
 
-   let result = count_digit(fewest_zeros, 1) * count_digit(fewest_zeros, 2);
+    let result = count_digit(fewest_zeros, 1) * count_digit(fewest_zeros, 2);
 
-    println!("{}", result);
+    println!("part 1: {}", result);
+    let layers: Vec<&[u32]> = nums
+        .chunks(layer_size)
+        .collect::<Vec<&[u32]>>();
+
+    let mut image: Vec<Vec<u32>> = vec![];
+    for layer in layers {
+        // initialize
+        for i in 0..height {
+            match image.get(i) {
+                Some(_) => (),
+                None => image.push(vec![])
+            }
+            for j in 0..width {
+                let mychar = layer[i * width + j];
+                let result = image[i].get(j);
+                if result == None {
+                    image[i].push(mychar)
+                }
+                else {
+                    let newchar = match image[i][j] {
+                        0 => 0,
+                        1 => 1,
+                        2 => mychar,
+                        _ => panic!()
+                    };
+                    image[i as usize][j] = newchar;
+                }
+            }
+        }
+    }
+
+
+    println!("part 2:");
+    for line in image {
+        for a in line {
+            match a {
+                1 => print!("O"),
+                0 => print!("_"),
+                _ => panic!()
+            }
+        }
+        print!("\n");
+    }
 }
